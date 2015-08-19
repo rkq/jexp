28c46819f3fef46cc95ddf1e9acf22cf625d04b7
Rick Qiu <qiudejun@gmail.com>
2015-08-19 22:31:48 +0800

    something about unit test

--- ut/src/test/java/com/github/rkq/jexp/ut/test/CalculatorTest.java
+++ ut/src/test/java/com/github/rkq/jexp/ut/test/CalculatorTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
  */
 public class CalculatorTest {
     @Test
-    public void evaluatesExpression() {
+    public void testEvaluateExpression() {
         Calculator calculator = new Calculator();
         int sum = calculator.evaluate("1+2+3");
         Assert.assertEquals(6, sum);
